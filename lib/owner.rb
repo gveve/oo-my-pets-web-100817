482c545a29ad3a25e6a7a9a5ab664d00b6c77504
github username <github email address>
2017-10-14 19:44:06 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
   # code goes here
+  attr_accessor :pets, :name
+  @ pets hash? = {fishes: [], cats: [], dogs: []}
+
+  def initialize(pets)
+    @pets = pets
+  end
+  cen buy pet
+  def buy_(name)
+    @name = name
+    
+  end
+
+  can set name of pet
+
+  change mood
+
+  sell all pets
 end

52c4ff9b304534d91a840c47d74b15d42acf7bfe
github username <github email address>
2017-10-14 21:14:16 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,20 +1,80 @@
 class Owner
-  # code goes here
   attr_accessor :pets, :name
-  @ pets hash? = {fishes: [], cats: [], dogs: []}
+  attr_reader :species
 
-  def initialize(pets)
-    @pets = pets
+  @@owners = []
+
+
+
+  def initialize(owner)
+    @name = owner
+    @@owners << self
+    @species = "human"
+    @@pets = {:fishes=>[], :dogs=>[], :cats=>[]}
   end
-  cen buy pet
-  def buy_(name)
-    @name = name
-    
+
+  def self.all
+    @@owners
   end
 
-  can set name of pet
+  def self.reset_all
+    @@owners.clear
+  end
 
-  change mood
+  def self.count
+    @@owners.length
+  end
 
-  sell all pets
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def name
+    @name
+  end
+
+  def pets
+    @@pets
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    pets[:fishes] <<(fish)
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    pets[:cats]<<(cat)
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    pets[:dogs]<<(dog)
+  end
+
+  def walk_dogs
+    pets[:dogs].each{|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    pets[:cats].each{|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    pets[:fishes].each{|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+     pets.each do |species, pet|
+       pet.each do |name|
+         name.mood = "nervous"
+       end
+       pet.clear
+     end
+   end
+
+  def list_pets
+    return "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
+
 end
